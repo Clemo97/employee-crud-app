356c585ef5d7a30eda3fccf8323bf3514bcb652c
Clemo97 <lumumbaclement@gmail.com>
2023-01-14 11:57:14 +0300

    fix errors

--- crudexample/settings.py
+++ crudexample/settings.py
@@ -18,6 +18,7 @@ import sys
 import dj_database_url
 from decouple import config,Csv
 from dotenv import load_dotenv
+import django_heroku
 
 from datetime import timedelta
 from django.contrib.messages import constants as messages
@@ -63,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'crudexample.urls'
@@ -148,10 +150,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
